cafe12168fd96b6296b1a0ae2c50c9072c66e9ac
MatsumotoMorami <asakurasayori@gmail.com>
2024-10-15 17:35:28 +0800

    1

--- components/frontpage-components.tsx
+++ components/frontpage-components.tsx
@@ -1,7 +1,10 @@
 export default function FrontpageComponentSet() {
     return (
         <div>
-            
+            <img className="fixed" src="/texture/frontpage/pattern.png"></img>
+            <img className="fixed w-[100vw] -translate-y-[100%] top-[50vh]" src="/texture/frontpage/pic.png"></img>
+            <img className="fixed" src="/texture/frontpage/pattern2.png"></img>
+            <img className="fixed -translate-x-1/2 -translate-y-1/2 top-[50vh] left-[50vw] fc-codeam rounded-3xl w-[35vw] " src="/texture/frontpage/codeam.png"></img>
         </div>
     )
 }
--- components/whole-page-export.tsx
+++ components/whole-page-export.tsx
@@ -1,8 +1,17 @@
+import { ReactNode } from "react";
 import Header from "./header";
 
-export default function Page() {
+export function Background() {
+    return (
+        <div className="fixed w-[100vw] h-[100vh] bg"></div>
+    )
+}
+
+export default function Page({ children }: { children: ReactNode }) {
     return (
         <div>
+            <Background></Background>
+            {children}
             <Header></Header>
         </div>
     )
