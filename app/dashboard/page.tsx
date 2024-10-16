1cb8a49395725ad5f30f69aaf04e41867a2887cf
Kohakuwu <asakurasayori@gmail.com>
2024-10-16 16:03:11 +0800

    1

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import FrontpageComponentSet from "@/components/frontpage-components";
+import Page from "@/components/whole-page-export";
+
+export default function App() {
+    return (
+        <Page>
+            <FrontpageComponentSet></FrontpageComponentSet>
+        </Page>
+    )
+}
