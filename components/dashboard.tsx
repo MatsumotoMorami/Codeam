f661413833696e99aca87ac47531daaaad959dae
unknown <3098805300@qq.com>
2024-10-16 22:27:22 +0800

    timeline

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -1,3 +1,5 @@
+import BasicTimeline from "./history-timeline"
+
 export default function DashboardComponentSet() {
     return (
         <div>
@@ -18,7 +20,7 @@ export function History() {
     return (
         <div className="w-full h-full p-[2vw] flex flex-row">
             <div className="text-[var(--blue)] text-3xl mr-[4vw] w-[10vw]  text-center">History</div>
-            <div className="absolute left-[14vw] h-[calc(28vh-4vw)] bg-[var(--blue)] w-[3px]"></div>
+            <div className="absolute left-[10vw] h-[calc(28vh-4vw)] w-[3px]"><BasicTimeline></BasicTimeline></div>
         </div>
     )
 }
