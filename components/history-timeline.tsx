d34b9cfa43291c9205c591e200de9c68c857ff58
MatsumotoMorami <asakurasayori@gmail.com>
2024-10-17 12:44:47 +0800

    1

--- components/history-timeline.tsx
+++ components/history-timeline.tsx
@@ -5,32 +5,70 @@ import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
+import { TimelineOppositeContent } from '@mui/lab';
+import { createTheme, ThemeProvider, Typography } from '@mui/material';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import LaptopMacIcon from '@mui/icons-material/LaptopMac';
+import HotelIcon from '@mui/icons-material/Hotel';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import { ThemeContext } from 'styled-components';
 
 export default function HistoryTimeline() {
+    const HistoryData: HistoryDataType[] = [
+        {
+            time: "2024.10.1 12:34:56",
+            operator: "Morami",
+            title: "睡觉",
+            content: "开睡！"
+        },
+        {
+            time: "2024.10.1 12:34:56",
+            operator: "Morami",
+            title: "睡觉",
+            content: "开睡！"
+        },
+        {
+            time: "2024.10.1 12:34:56",
+            operator: "Morami",
+            title: "睡觉",
+            content: "开睡！"
+        },
+        {
+            time: "2024.10.1 12:34:56",
+            operator: "Morami",
+            title: "睡觉",
+            content: "开睡！"
+        }
+    ]
+    const sx = {
+        '& .MuiTimelineItem-root:before': {
+            flex: 0,
+            padding: 0,
+        }
+    }
     return (
-        <div className='text-[var(--blue)]'>
-            <Timeline>
-                <TimelineItem>
-                    <TimelineSeparator>
-                        <TimelineDot color='secondary' />
-                        <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>Eat</TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                    <TimelineSeparator>
-                        <TimelineDot />
-                        <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>Code</TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                    <TimelineSeparator>
-                        <TimelineDot />
-                    </TimelineSeparator>
-                    <TimelineContent>Sleep</TimelineContent>
-                </TimelineItem>
-            </Timeline>
-        </div>
+
+        <Timeline>
+            <TimelineItem>
+                <TimelineSeparator>
+                    <TimelineDot />
+                    <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent>Eat</TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+                <TimelineSeparator>
+                    <TimelineDot />
+                    <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent>Code</TimelineContent>
+            </TimelineItem>
+            {/* <TimelineItem>
+                <TimelineSeparator>
+                    <TimelineDot />
+                </TimelineSeparator>
+                <TimelineContent>Sleep</TimelineContent>
+            </TimelineItem> */}
+        </Timeline>
     );
 }
