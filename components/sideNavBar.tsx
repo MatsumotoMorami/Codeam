f7772cfe4e3282510e292ebf69f9ac4e072fff7c
Kohakuwu <asakurasayori@gmail.com>
2024-10-20 03:07:21 +0800

    1

--- components/sideNavBar.tsx
+++ components/sideNavBar.tsx
@@ -5,6 +5,10 @@ import MagicIcon from '@rsuite/icons/legacy/Magic';
 import GearCircleIcon from '@rsuite/icons/legacy/GearCircle';
 import NavToggle from './NavToggle';
 import { useEffect, useState } from 'react';
+import { Icon } from './icon';
+import { TextImage } from '@rsuite/icons';
+import PageIcon from '@rsuite/icons/Page';
+import MessageIcon from '@rsuite/icons/Message';
 
 const { getHeight, on } = DOMHelper;
 
@@ -16,12 +20,12 @@ export default function SideNavBar({ expanded, setExpand, nbs }) {
                     <Nav.Item eventKey="1" icon={<DashboardIcon />}>
                         Dashboard
                     </Nav.Item>
-                    <Nav.Menu onClick={() => !expanded ? setExpand(!expanded) : {}} eventKey="2" title='Knowledge & WIKI' icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">  <path d="M96 0C43 0 0 43 0 96L0 416c0 53 43 96 96 96l288 0 32 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l0-64c17.7 0 32-14.3 32-32l0-320c0-17.7-14.3-32-32-32L384 0 96 0zm0 384l256 0 0 64L96 448c-17.7 0-32-14.3-32-32s14.3-32 32-32zm32-240c0-8.8 7.2-16 16-16l192 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-192 0c-8.8 0-16-7.2-16-16zm16 48l192 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-192 0c-8.8 0-16-7.2-16-16s7.2-16 16-16z" /></svg>}>
+                    <Nav.Menu onClick={() => !expanded ? setExpand(!expanded) : {}} eventKey="2" title='Knowledge & WIKI' icon={<TextImage />}>
                         <Nav.Item eventKey="2-1">Visualization</Nav.Item>
                         <Nav.Item eventKey="2-2">Knowledge</Nav.Item>
                     </Nav.Menu>
-                    <Nav.Item eventKey="3" icon={<MagicIcon />}>Files</Nav.Item>
-                    <Nav.Item eventKey="4" icon={<GroupIcon />}>Discussion</Nav.Item>
+                    <Nav.Item eventKey="3" icon={<PageIcon />}>Files</Nav.Item>
+                    <Nav.Item eventKey="4" icon={<MessageIcon />}>Discussion</Nav.Item>
                 </Nav>
             </Sidenav.Body>
             <NavToggle expand={expanded} onChange={() => setExpand(!expanded)} />
